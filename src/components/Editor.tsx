7023a1eda57bafbac32d5392ea6b19794c4c094c
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-02-16 16:25:59 -0600

    allow props for custom editor

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,26 +1,36 @@
 import { useRef, useState, useEffect } from 'react';
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 import styles from './Editor.module.css';
+import { EditorProps } from '@monaco-editor/react';
 
 // alernative implementation of the Editor component
-export const Editor = () => {
-	const [editor, setEditor] = useState<monaco.editor.IStandaloneCodeEditor | null>(null);
-	const monacoEl = useRef(null);
+export const Editor = (props: EditorProps) => {
+  const [editor, setEditor] =
+    useState<monaco.editor.IStandaloneCodeEditor | null>(null);
+  const monacoEl = useRef(null);
 
-	useEffect(() => {
-		if (monacoEl) {
-			setEditor((editor) => {
-				if (editor) return editor;
+  useEffect(() => {
+    if (monacoEl) {
+      setEditor((editor) => {
+        if (editor) return editor;
 
-				return monaco.editor.create(monacoEl.current!, {
-					value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join('\n'),
-					language: 'typescript'
-				});
-			});
-		}
+        return monaco.editor.create(monacoEl.current!, {
+          ...props,
+          value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join(
+            '\n'
+          ),
+          language: 'typescript',
+        });
+      });
+    }
 
-		return () => editor?.dispose();
-	}, [monacoEl.current]);
+    return () => editor?.dispose();
+  }, [monacoEl.current]);
 
-	return <div className={styles.Editor} ref={monacoEl}></div>;
+  return (
+    <div
+      className={styles.Editor}
+      ref={monacoEl}
+    />
+  );
 };
