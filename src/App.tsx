9240c375b4b3def8abff8d333de3c2c9f63db87c
Fernando González Tostado <68973307+esponges@users.noreply.github.com>
2024-02-16 16:25:49 -0600

    add extra languages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import Editor, { type Monaco } from '@monaco-editor/react';
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 import EditorStyles from './components/Editor.module.css';
 
 function App() {
+  const [language, setLanguage] = useState('typescript');
   const editorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
 
   function handleEditorDidMount(
@@ -23,9 +24,19 @@ function App() {
     <>
       <div className={EditorStyles.Editor}>
         <button onClick={showValue}>Show value</button>
+        <select value={language} onChange={(e) => setLanguage(e.target.value)}>
+          <option value='javascript'>JavaScript</option>
+          <option value='typescript'>TypeScript</option>
+          <option value='python'>Python</option>
+          <option value='java'>Java</option>
+          <option value='csharp'>C#</option>
+          <option value='go'>Go</option>
+        </select>
         <Editor
           height='90vh'
-          defaultLanguage='javascript'
+          theme='vs-dark'
+          defaultLanguage='typescript'
+          language={language}
           defaultValue='// some comment'
           onMount={handleEditorDidMount}
         />
